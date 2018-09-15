1db62fe0c7655430c9b4942356cee17b540174bf
Yi Lu <yil@mit.edu>
2018-09-15 17:47:46 -0400

    add search and update to calvin transaction

--- protocol/Calvin/CalvinTransaction.h
+++ protocol/Calvin/CalvinTransaction.h
@@ -36,19 +36,70 @@ public:
 
   template <class KeyType, class ValueType>
   void search_local_index(std::size_t table_id, std::size_t partition_id,
-                          const KeyType &key, ValueType &value) {}
+                          const KeyType &key, ValueType &value) {
+    CalvinRWKey readKey;
+
+    readKey.set_table_id(table_id);
+    readKey.set_partition_id(partition_id);
+
+    readKey.set_key(&key);
+    readKey.set_value(&value);
+
+    readKey.set_local_index_read_bit();
+    readKey.set_read_lock_bit();
+
+    add_to_read_set(readKey);
+  }
 
   template <class KeyType, class ValueType>
   void search_for_read(std::size_t table_id, std::size_t partition_id,
-                       const KeyType &key, ValueType &value) {}
+                       const KeyType &key, ValueType &value) {
+
+    CalvinRWKey readKey;
+
+    readKey.set_table_id(table_id);
+    readKey.set_partition_id(partition_id);
+
+    readKey.set_key(&key);
+    readKey.set_value(&value);
+
+    readKey.set_read_lock_bit();
+
+    add_to_read_set(readKey);
+  }
 
   template <class KeyType, class ValueType>
   void search_for_update(std::size_t table_id, std::size_t partition_id,
-                         const KeyType &key, ValueType &value) {}
+                         const KeyType &key, ValueType &value) {
+
+    CalvinRWKey readKey;
+
+    readKey.set_table_id(table_id);
+    readKey.set_partition_id(partition_id);
+
+    readKey.set_key(&key);
+    readKey.set_value(&value);
+
+    readKey.set_write_lock_bit();
+
+    add_to_read_set(readKey);
+  }
 
   template <class KeyType, class ValueType>
   void update(std::size_t table_id, std::size_t partition_id,
-              const KeyType &key, const ValueType &value) {}
+              const KeyType &key, const ValueType &value) {
+
+    CalvinRWKey writeKey;
+
+    writeKey.set_table_id(table_id);
+    writeKey.set_partition_id(partition_id);
+
+    writeKey.set_key(&key);
+    // the object pointed by value will not be updated
+    writeKey.set_value(const_cast<ValueType *>(&value));
+
+    add_to_write_set(writeKey);
+  }
 
   std::size_t add_to_read_set(const CalvinRWKey &key) {
     readSet.push_back(key);
