5d2498aaf480f19799a28a7ea5e80b5bbe1733ff
Yi Lu <luyi0619@gmail.com>
2018-09-20 15:14:12 -0700

    add ycsb

--- bench_ycsb.cpp
+++ bench_ycsb.cpp
@@ -1,10 +1,5 @@
-#include "benchmark/tpcc/Database.h"
-#include "benchmark/tpcc/Workload.h"
+#include "benchmark/ycsb/Database.h"
 #include "core/Coordinator.h"
-#include "protocol/Calvin/CalvinTransaction.h"
-#include "protocol/Scar/ScarTransaction.h"
-#include "protocol/Silo/SiloTransaction.h"
-#include "protocol/TwoPL/TwoPLTransaction.h"
 #include <boost/algorithm/string.hpp>
 #include <gflags/gflags.h>
 #include <glog/logging.h>
@@ -21,9 +16,6 @@ DEFINE_string(protocol, "RStore", "transaction protocol");
 DEFINE_string(replica_group, "1,3", "calvin replica group");
 DEFINE_string(partitioner, "hash", "database partitioner (hash, hash2, pb)");
 DEFINE_bool(operation_replication, false, "use operation replication");
-DEFINE_string(query, "neworder", "tpcc query, mixed, neworder, payment");
-DEFINE_int32(neworder_dist, 10, "new order distributed.");
-DEFINE_int32(payment_dist, 15, "payment distributed.");
 
 // ./main --logtostderr=1 --id=1 --servers="127.0.0.1:10010;127.0.0.1:10011"
 // cmake -DCMAKE_BUILD_TYPE=Release
@@ -38,7 +30,7 @@ int main(int argc, char *argv[]) {
   boost::algorithm::split(peers, FLAGS_servers, boost::is_any_of(";"));
 
   int n = FLAGS_threads;
-  scar::tpcc::Context context;
+  scar::ycsb::Context context;
   context.protocol = FLAGS_protocol;
   context.coordinator_num = peers.size();
   context.batch_size = FLAGS_batch_size;
@@ -50,28 +42,16 @@ int main(int argc, char *argv[]) {
   context.operation_replication = FLAGS_operation_replication;
   context.partitioner = FLAGS_partitioner;
 
-  if (FLAGS_query == "mixed") {
-    context.workloadType = scar::tpcc::TPCCWorkloadType ::MIXED;
-  } else if (FLAGS_query == "neworder") {
-    context.workloadType = scar::tpcc::TPCCWorkloadType ::NEW_ORDER_ONLY;
-  } else if (FLAGS_query == "payment") {
-    context.workloadType = scar::tpcc::TPCCWorkloadType ::PAYMENT_ONLY;
-  } else {
-    CHECK(false);
-  }
-
-  context.newOrderCrossPartitionProbability = FLAGS_neworder_dist;
-  context.paymentCrossPartitionProbability = FLAGS_payment_dist;
 
   // only create coordinator for tpc-c
   std::unordered_set<std::string> protocols = {
-      "Silo", "SiloGC", "Scar", "RStore", "TwoPL", "TwoPLGC", "Calvin"};
+          "Silo", "SiloGC", "Scar", "RStore", "TwoPL", "TwoPLGC", "Calvin"};
   std::unordered_set<std::string> silo_protocols = {"Silo", "SiloGC", "RStore"};
   std::unordered_set<std::string> scar_protocols = {"Scar"};
   std::unordered_set<std::string> twopl_protocols = {"TwoPL", "TwoPLGC"};
 
   using MetaDataType = std::atomic<uint64_t>;
-  scar::tpcc::Database<MetaDataType> db;
+  scar::ycsb::Database<MetaDataType> db;
   db.initialize(context, context.partition_num, n);
 
   auto c = std::make_unique<scar::Coordinator>(FLAGS_id, peers, db, context);
