b96d3f1936f5e7f8abab48525c4e807e02dc2fc3
Yi Lu <luyi0619@gmail.com>
2019-04-15 21:41:31 -0400

    fix bug in random seed

--- benchmark/ycsb/Transaction.h
+++ benchmark/ycsb/Transaction.h
@@ -66,26 +66,27 @@ public:
       if (query.UPDATE[i]) {
 
         if (this->execution_phase) {
+          RandomType local_random;
           storage.ycsb_values[i].Y_F01.assign(
-              random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
+              local_random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
           storage.ycsb_values[i].Y_F02.assign(
-              random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
+              local_random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
           storage.ycsb_values[i].Y_F03.assign(
-              random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
+              local_random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
           storage.ycsb_values[i].Y_F04.assign(
-              random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
+              local_random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
           storage.ycsb_values[i].Y_F05.assign(
-              random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
+              local_random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
           storage.ycsb_values[i].Y_F06.assign(
-              random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
+              local_random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
           storage.ycsb_values[i].Y_F07.assign(
-              random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
+              local_random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
           storage.ycsb_values[i].Y_F08.assign(
-              random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
+              local_random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
           storage.ycsb_values[i].Y_F09.assign(
-              random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
+              local_random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
           storage.ycsb_values[i].Y_F10.assign(
-              random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
+              local_random.a_string(YCSB_FIELD_SIZE, YCSB_FIELD_SIZE));
         }
 
         this->update(ycsbTableID, context.getPartitionID(key),
