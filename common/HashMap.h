d6c33a76e2e40fecef9eb578df44cf8608d4d934
Yi Lu <yil@mit.edu>
2019-09-02 14:33:48 -0400

    some renames in hashmap

--- common/HashMap.h
+++ common/HashMap.h
@@ -12,12 +12,15 @@
 namespace scar {
 
 template <std::size_t N, class KeyType, class ValueType> class HashMap {
-public:
-  using hasher = typename std::unordered_map<KeyType, ValueType>::hasher;
 
+public:
+  using HashMapType = std::unordered_map<KeyType, ValueType>;
+  using HasherType = typename HashMapType::hasher;
+
+public:
   bool remove(const KeyType &key) {
-    return _applyAt(
-        [&key](std::unordered_map<KeyType, ValueType> &map) {
+    return apply(
+        [&key](HashMapType &map) {
           auto it = map.find(key);
           if (it == map.end()) {
             return false;
@@ -26,91 +29,86 @@ public:
             return true;
           }
         },
-        bucketNo(key));
+        bucket_number(key));
   }
 
   bool contains(const KeyType &key) {
-    return _applyAt(
-        [&key](const std::unordered_map<KeyType, ValueType> &map) {
-          return map.find(key) != map.end();
-        },
-        bucketNo(key));
+    return apply(
+        [&key](const HashMapType &map) { return map.find(key) != map.end(); },
+        bucket_number(key));
   }
 
   bool insert(const KeyType &key, const ValueType &value) {
-    return _applyAt(
-        [&key, &value](std::unordered_map<KeyType, ValueType> &map) {
+    return apply(
+        [&key, &value](HashMapType &map) {
           if (map.find(key) != map.end()) {
             return false;
           }
           map[key] = value;
           return true;
         },
-        bucketNo(key));
+        bucket_number(key));
   }
 
   ValueType &operator[](const KeyType &key) {
-    return _applyAtRef(
-        [&key](std::unordered_map<KeyType, ValueType> &map) -> ValueType & {
-          return map[key];
-        },
-        bucketNo(key));
+    return apply_ref(
+        [&key](HashMapType &map) -> ValueType & { return map[key]; },
+        bucket_number(key));
   }
 
   std::size_t size() {
-    return _fold(0, [](std::size_t totalSize,
-                       const std::unordered_map<KeyType, ValueType> &map) {
+    return fold(0, [](std::size_t totalSize, const HashMapType &map) {
       return totalSize + map.size();
     });
   }
 
   void clear() {
-    _map([](std::unordered_map<KeyType, ValueType> &map) { map.clear(); });
+    map([](HashMapType &map) { map.clear(); });
   }
 
 private:
   template <class ApplyFunc>
-  auto &_applyAtRef(ApplyFunc applyFunc, std::size_t i) {
+  auto &apply_ref(ApplyFunc applyFunc, std::size_t i) {
     DCHECK(i < N) << "index " << i << " is greater than " << N;
-    locks_[i].lock();
-    auto &result = applyFunc(maps_[i]);
-    locks_[i].unlock();
+    locks[i].lock();
+    auto &result = applyFunc(maps[i]);
+    locks[i].unlock();
     return result;
   }
 
-  template <class ApplyFunc> auto _applyAt(ApplyFunc applyFunc, std::size_t i) {
+  template <class ApplyFunc> auto apply(ApplyFunc applyFunc, std::size_t i) {
     DCHECK(i < N) << "index " << i << " is greater than " << N;
-    locks_[i].lock();
-    auto result = applyFunc(maps_[i]);
-    locks_[i].unlock();
+    locks[i].lock();
+    auto result = applyFunc(maps[i]);
+    locks[i].unlock();
     return result;
   }
 
-  template <class MapFunc> void _map(MapFunc mapFunc) {
+  template <class MapFunc> void map(MapFunc mapFunc) {
     for (auto i = 0u; i < N; i++) {
-      locks_[i].lock();
-      mapFunc(maps_[i]);
-      locks_[i].unlock();
+      locks[i].lock();
+      mapFunc(maps[i]);
+      locks[i].unlock();
     }
   }
 
   template <class T, class FoldFunc>
-  auto _fold(const T &firstValue, FoldFunc foldFunc) {
+  auto fold(const T &firstValue, FoldFunc foldFunc) {
     T finalValue = firstValue;
     for (auto i = 0u; i < N; i++) {
-      locks_[i].lock();
-      finalValue = foldFunc(finalValue, maps_[i]);
-      locks_[i].unlock();
+      locks[i].lock();
+      finalValue = foldFunc(finalValue, maps[i]);
+      locks[i].unlock();
     }
     return finalValue;
   }
 
-  auto bucketNo(const KeyType &key) { return hasher_(key) % N; }
+  auto bucket_number(const KeyType &key) { return hasher(key) % N; }
 
 private:
-  hasher hasher_;
-  std::unordered_map<KeyType, ValueType> maps_[N];
-  SpinLock locks_[N];
+  HasherType hasher;
+  HashMapType maps[N];
+  SpinLock locks[N];
 };
 
 } // namespace scar
