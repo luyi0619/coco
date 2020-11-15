abd0db56e4959ad2170fcb28dbf2af7414ae6f3c
Yi Lu <yil@mit.edu>
2020-11-14 22:28:04 -0800

    delete deprecated protocols

--- core/factory/WorkerFactory.h
+++ core/factory/WorkerFactory.h
@@ -28,36 +28,11 @@
 #include "protocol/SiloGC/SiloGCExecutor.h"
 #include "protocol/SiloSI/SiloSI.h"
 #include "protocol/SiloSI/SiloSIExecutor.h"
-#include "protocol/TwoPLGC/TwoPLGC.h"
-#include "protocol/TwoPLGC/TwoPLGCExecutor.h"
-
-#include "protocol/Star/Star.h"
-#include "protocol/Star/StarExecutor.h"
-#include "protocol/Star/StarManager.h"
-
-#include "protocol/Calvin/Calvin.h"
-#include "protocol/Calvin/CalvinExecutor.h"
-#include "protocol/Calvin/CalvinManager.h"
-#include "protocol/Calvin/CalvinTransaction.h"
-
-#include "protocol/Bohm/Bohm.h"
-#include "protocol/Bohm/BohmExecutor.h"
-#include "protocol/Bohm/BohmManager.h"
-#include "protocol/Bohm/BohmTransaction.h"
 
 #include "protocol/Aria/Aria.h"
 #include "protocol/Aria/AriaExecutor.h"
 #include "protocol/Aria/AriaManager.h"
 #include "protocol/Aria/AriaTransaction.h"
-
-#include "protocol/AriaFB/AriaFB.h"
-#include "protocol/AriaFB/AriaFBExecutor.h"
-#include "protocol/AriaFB/AriaFBManager.h"
-#include "protocol/AriaFB/AriaFBTransaction.h"
-
-#include "protocol/Pwv/PwvExecutor.h"
-#include "protocol/Pwv/PwvManager.h"
-#include "protocol/Pwv/PwvTransaction.h"
 
 #include <unordered_set>
 
@@ -85,9 +60,9 @@ public:
   create_workers(std::size_t coordinator_id, Database &db,
                  const Context &context, std::atomic<bool> &stop_flag) {
 
-    std::unordered_set<std::string> protocols = {
-        "Silo",  "SiloGC",  "SiloSI", "Scar", "ScarGC", "ScarSI", "Star",
-        "TwoPL", "TwoPLGC", "Calvin", "Bohm", "Aria",   "AriaFB", "Pwv"};
+    std::unordered_set<std::string> protocols = {"Silo",  "SiloGC", "SiloSI",
+                                                 "Scar",  "ScarGC", "ScarSI",
+                                                 "TwoPL", "Aria"};
     CHECK(protocols.count(context.protocol) == 1);
 
     std::vector<std::shared_ptr<Worker>> workers;
@@ -191,28 +166,6 @@ public:
 
       workers.push_back(manager);
 
-    } else if (context.protocol == "Star") {
-
-      CHECK(context.partition_num %
-                (context.worker_num * context.coordinator_num) ==
-            0)
-          << "In Star, each partition is managed by only one thread.";
-
-      using TransactionType = coco::SiloTransaction;
-      using WorkloadType =
-          typename InferType<Context>::template WorkloadType<TransactionType>;
-
-      auto manager = std::make_shared<StarManager>(
-          coordinator_id, context.worker_num, context, stop_flag);
-
-      for (auto i = 0u; i < context.worker_num; i++) {
-        workers.push_back(std::make_shared<StarExecutor<WorkloadType>>(
-            coordinator_id, i, db, context, manager->batch_size,
-            manager->worker_status, manager->n_completed_workers,
-            manager->n_started_workers));
-      }
-      workers.push_back(manager);
-
     } else if (context.protocol == "TwoPL") {
 
       using TransactionType = coco::TwoPLTransaction;
@@ -225,76 +178,6 @@ public:
       for (auto i = 0u; i < context.worker_num; i++) {
         workers.push_back(std::make_shared<TwoPLExecutor<WorkloadType>>(
             coordinator_id, i, db, context, manager->worker_status,
-            manager->n_completed_workers, manager->n_started_workers));
-      }
-
-      workers.push_back(manager);
-    } else if (context.protocol == "TwoPLGC") {
-
-      using TransactionType = coco::TwoPLTransaction;
-      using WorkloadType =
-          typename InferType<Context>::template WorkloadType<TransactionType>;
-
-      auto manager = std::make_shared<group_commit::Manager>(
-          coordinator_id, context.worker_num, context, stop_flag);
-
-      for (auto i = 0u; i < context.worker_num; i++) {
-        workers.push_back(std::make_shared<TwoPLGCExecutor<WorkloadType>>(
-            coordinator_id, i, db, context, manager->worker_status,
-            manager->n_completed_workers, manager->n_started_workers));
-      }
-
-      workers.push_back(manager);
-    } else if (context.protocol == "Calvin") {
-
-      using TransactionType = coco::CalvinTransaction;
-      using WorkloadType =
-          typename InferType<Context>::template WorkloadType<TransactionType>;
-
-      // create manager
-
-      auto manager = std::make_shared<CalvinManager<WorkloadType>>(
-          coordinator_id, context.worker_num, db, context, stop_flag);
-
-      // create worker
-
-      std::vector<CalvinExecutor<WorkloadType> *> all_executors;
-
-      for (auto i = 0u; i < context.worker_num; i++) {
-
-        auto w = std::make_shared<CalvinExecutor<WorkloadType>>(
-            coordinator_id, i, db, context, manager->transactions,
-            manager->storages, manager->lock_manager_status,
-            manager->worker_status, manager->n_completed_workers,
-            manager->n_started_workers);
-        workers.push_back(w);
-        manager->add_worker(w);
-        all_executors.push_back(w.get());
-      }
-      // push manager to workers
-      workers.push_back(manager);
-
-      for (auto i = 0u; i < context.worker_num; i++) {
-        static_cast<CalvinExecutor<WorkloadType> *>(workers[i].get())
-            ->set_all_executors(all_executors);
-      }
-    } else if (context.protocol == "Bohm") {
-
-      using TransactionType = coco::BohmTransaction;
-      using WorkloadType =
-          typename InferType<Context>::template WorkloadType<TransactionType>;
-
-      // create manager
-
-      auto manager = std::make_shared<BohmManager<WorkloadType>>(
-          coordinator_id, context.worker_num, db, context, stop_flag);
-
-      // create worker
-
-      for (auto i = 0u; i < context.worker_num; i++) {
-        workers.push_back(std::make_shared<BohmExecutor<WorkloadType>>(
-            coordinator_id, i, db, context, manager->transactions,
-            manager->storages, manager->epoch, manager->worker_status,
             manager->n_completed_workers, manager->n_started_workers));
       }
 
@@ -321,56 +204,6 @@ public:
       }
 
       workers.push_back(manager);
-    } else if (context.protocol == "AriaFB") {
-
-      using TransactionType = coco::AriaFBTransaction;
-      using WorkloadType =
-          typename InferType<Context>::template WorkloadType<TransactionType>;
-
-      // create manager
-
-      auto manager = std::make_shared<AriaFBManager<WorkloadType>>(
-          coordinator_id, context.worker_num, db, context, stop_flag);
-
-      // create worker
-
-      std::vector<AriaFBExecutor<WorkloadType> *> all_executors;
-
-      for (auto i = 0u; i < context.worker_num; i++) {
-        auto w = std::make_shared<AriaFBExecutor<WorkloadType>>(
-            coordinator_id, i, db, context, manager->transactions,
-            manager->partition_ids, manager->storages, manager->epoch,
-            manager->lock_manager_status, manager->worker_status,
-            manager->total_abort, manager->n_completed_workers,
-            manager->n_started_workers);
-        workers.push_back(w);
-        manager->add_worker(w);
-        all_executors.push_back(w.get());
-      }
-
-      // push manager to workers
-      workers.push_back(manager);
-
-      for (auto i = 0u; i < context.worker_num; i++) {
-        static_cast<AriaFBExecutor<WorkloadType> *>(workers[i].get())
-            ->set_all_executors(all_executors);
-      }
-    } else if (context.protocol == "Pwv") {
-
-      // create manager
-      auto manager = std::make_shared<PwvManager<Database>>(
-          coordinator_id, context.worker_num, db, context, stop_flag);
-
-      // create worker
-      for (auto i = 0u; i < context.worker_num; i++) {
-        workers.push_back(std::make_shared<PwvExecutor<Database>>(
-            coordinator_id, i, db, context, manager->transactions,
-            manager->storages, manager->epoch, manager->worker_status,
-            manager->n_completed_workers, manager->n_started_workers));
-      }
-
-      workers.push_back(manager);
-
     } else {
       CHECK(false) << "protocol: " << context.protocol << " is not supported.";
     }
